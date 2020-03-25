79b1c5036a243e9de954acf4af9727a02cfb44bf
Murat Bilginer <muratbilginer09@gmail.com>
2020-03-25 22:52:20 +0300

    Initial commit

---
+++ Forma_sekiller_cizme/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Forma_sekiller_cizme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ComboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // ComboBox1
+            // 
+            this.ComboBox1.FormattingEnabled = true;
+            this.ComboBox1.Items.AddRange(new object[] {
+            "Çember",
+            "Elips",
+            "Kare",
+            "Dikdörtgen",
+            "Yazı yaz",
+            "Yay",
+            "Serbest Yay",
+            "Serbest Çizim",
+            "Çizgi",
+            "Çeyrek Çember",
+            "Çokgen"});
+            this.ComboBox1.Location = new System.Drawing.Point(12, 13);
+            this.ComboBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.ComboBox1.Name = "ComboBox1";
+            this.ComboBox1.Size = new System.Drawing.Size(458, 24);
+            this.ComboBox1.TabIndex = 3;
+            this.ComboBox1.Text = "Seçiminiz Yapınız...";
+            this.ComboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(482, 309);
+            this.Controls.Add(this.ComboBox1);
+            this.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Geometrik Şekil Çizme";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.ComboBox ComboBox1;
+    }
+}
+
---
+++ Forma_sekiller_cizme/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Forma_sekiller_cizme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Graphics g; 
+        Font fontum = new Font("tahoma", 15); 
+        SolidBrush  fircam = new SolidBrush(Color.Blue); 
+        Pen  kalemim = new Pen(Color.Blue, 2); 
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (ComboBox1.Text)
+            {//Comboda seçili olan
+                case "Çember"://Çember ise
+                    {
+                        g = this.CreateGraphics();
+                        g.Clear(this.BackColor);  
+                        g.DrawEllipse(kalemim, 120, 100, 80, 80); 
+                        g.Dispose(); 
+                        break;
+                    }
+                case "Elips":
+                    {
+                        g = this.CreateGraphics();
+                        g.Clear(this.BackColor);
+                        g.DrawEllipse(kalemim, 120, 100, 100, 65);
+                        g.Dispose();
+                        break;
+                    }
+                case "Kare":
+                    {
+                        g = this.CreateGraphics();
+                        g.Clear(this.BackColor);
+                        g.DrawRectangle(kalemim, 120, 100, 80, 80);
+                        g.Dispose();
+                        break;
+                    }
+                case "Dikdörtgen":
+                    {
+                        g = this.CreateGraphics();
+                        g.Clear(this.BackColor);
+                        g.DrawRectangle(kalemim, 120, 100, 100, 65);
+                        g.Dispose();
+                        break;
+                    }
+                case "Yazı yaz":
+                    {
+                        g = this.CreateGraphics();
+                        g.Clear(this.BackColor);
+                        g.DrawString("Murat Bilginer Kişisel Web Sayfası", fontum, fircam, 70, 100);
+                        g.Dispose();
+                        break;
+                    }
+                case "Yay":
+                    {
+                        g = this.CreateGraphics();
+                        g.Clear(this.BackColor);
+                        g.DrawArc(kalemim, 120, 80, 50, 50, 90, 250);
+                        g.Dispose();
+                        break;
+                    }
+                case "Serbest Yay":
+                    {
+                        g = this.CreateGraphics();
+                        g.Clear(this.BackColor);
+                        g.DrawBezier(kalemim, 100, 75, 135, 100, 170, 100, 200, 75);
+                        g.Dispose();
+                        break;
+                    }
+                case "Serbest Çizim":
+                    {
+                        g = this.CreateGraphics();
+                        g.Clear(this.BackColor);
+                        Point[] p1 = { new Point(100, 70), new Point(125, 100), new Point(150, 150), new Point(200, 60) };
+                        g.DrawCurve(kalemim, p1, 1);
+                        g.Dispose();
+                        break;
+                    }
+                case "Çizgi":
+                    {
+                        g = this.CreateGraphics();
+                        g.Clear(this.BackColor);
+                        g.DrawLine(kalemim, 100, 100, 300, 100);
+                        g.Dispose();
+                        break;
+                    }
+                case "Çeyrek Çember":
+                    {
+                        g = this.CreateGraphics();
+                        g.Clear(this.BackColor);
+                        g.DrawPie(kalemim, 100, 100, 150, 100, 200, 100);
+                        g.Dispose();
+                        break;
+                    }
+                case "Çokgen":
+                    {
+                        g = this.CreateGraphics();
+                        g.Clear(this.BackColor);
+                        Point[] p2 = { new Point(150, 100), new Point(200, 150), new Point(200, 200), new Point(150, 250), new Point(100, 150), new Point(100, 110) };
+                        g.DrawPolygon(kalemim, p2);
+                        g.Dispose();
+                        break;
+                    }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
